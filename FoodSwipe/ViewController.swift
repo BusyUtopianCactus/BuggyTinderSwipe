30db2d55e384450db24c6e4ff7ad90c23a730ed9
Daniel Basman <dbasman2@gmail.com>
2018-08-09 23:56:33 -0700

    added reset button to both pages- Resubmit to app store

--- FoodSwipe/ViewController.swift
+++ FoodSwipe/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
 
 
     // MARK: Properties
-    var foodChoiceRestart = [String?]()
+    var foodChoiceRestart = ["Chinese", "Mexican", "Italian", "Japanese", "Mediterranean", "French", "Thai", "Spanish", "Indian", "Greek", "BBQ", "Burgers", "Sandwich", "Seafood", "Pizza", "Steak"]
     var deletedItemArray = [String]()
     var deletedItem: String = ""
     let locationManager = CLLocationManager()
-
+    var restarterArray = [String]()
     
     // MARK: Undo button
     @IBAction func restartChoiceClicked(_ sender: UIBarButtonItem) {
@@ -96,6 +96,92 @@ class ViewController: UIViewController {
     }
     
 
+    @IBAction func restartAppButton(_ sender: UIBarButtonItem) {
+        if didSwipe == false {
+            didSwipe = true
+            
+        }
+        
+//        var foodChoices1 = ["Chinese", "Mexican", "Italian", "Japanese", "Mediterranean", "French", "Thai", "Spanish", "Indian", "Greek", "BBQ", "Burgers", "Sandwich", "Seafood", "Pizza", "Steak"]
+        
+        foodChoices.append(contentsOf: deletedItemArray)
+        print(foodChoices)
+        foodChoices = foodChoices.filter{$0 != "Chinese"}
+        foodChoices.insert("Chinese", at: 0)
+        foodChoices = foodChoices.filter{$0 != "Mexican"}
+        foodChoices.insert("Mexican", at: 1)
+        foodChoices = foodChoices.filter{$0 != "Italian"}
+        foodChoices.insert("Italian", at: 2)
+        foodChoices = foodChoices.filter{$0 != "Japanese"}
+        foodChoices.insert("Japanese", at: 3)
+        foodChoices = foodChoices.filter{$0 != "Mediterranean"}
+        foodChoices.insert("Mediterranean", at: 4)
+        foodChoices = foodChoices.filter{$0 != "French"}
+        foodChoices.insert("French", at: 5)
+        foodChoices = foodChoices.filter{$0 != "Thai"}
+        foodChoices.insert("Thai", at: 6)
+        foodChoices = foodChoices.filter{$0 != "Spanish"}
+        foodChoices.insert("Spanish", at: 7)
+        foodChoices = foodChoices.filter{$0 != "Indian"}
+        foodChoices.insert("Indian", at: 8)
+        foodChoices = foodChoices.filter{$0 != "Greek"}
+        foodChoices.insert("Greek", at: 9)
+        foodChoices = foodChoices.filter{$0 != "BBQ"}
+        foodChoices.insert("BBQ", at: 10)
+        foodChoices = foodChoices.filter{$0 != "Burgers"}
+        foodChoices.insert("Burgers", at: 11)
+        foodChoices = foodChoices.filter{$0 != "Sandwich"}
+        foodChoices.insert("Sandwich", at: 12)
+        foodChoices = foodChoices.filter{$0 != "Seafood"}
+        foodChoices.insert("Seafood", at: 13)
+        foodChoices = foodChoices.filter{$0 != "Pizza"}
+        foodChoices.insert("Pizza", at: 14)
+        foodChoices = foodChoices.filter{$0 != "Steak"}
+        foodChoices.insert("Steak", at: 15)
+        
+        print(foodChoices)
+        
+//        var foodChoices = ["Chinese", "Mexican", "Italian", "Japanese", "Mediterranean", "French", "Thai", "Spanish", "Indian", "Greek", "BBQ", "Burgers", "Sandwich", "Seafood", "Pizza", "Steak"]
+        
+        //foodChoices = fullfoodChoices()
+        //foodChoices.append(contentsOf: deletedItemArray)
+//
+//        //let restarterArray = foodChoices.append(contentsOf: deletedItemArray)
+//        if restarterArray.count >= 16 {
+//            restartWholeApp.isEnabled = false
+//        } else {
+//            restartWholeApp.isEnabled = true
+//        }
+//        print(restarterArray.count)
+//        print("This is: \(restarterArray)")
+
+        
+        
+        
+        
+        if foodChoices.count >= 16 {
+            restartWholeApp.isEnabled = false
+                    }
+        if foodChoices.count <= 16 {
+            restartWholeApp.isEnabled = true
+        }
+        deletedItemArray = []
+        print("Guess what \(deletedItemArray)")
+        index = -1
+        updateIndex()
+        
+        generateAnswer()
+//        foodPic.image = UIImage(named: foodChoices[1])
+//        foodCategory.text = foodChoices[1]
+
+        approvedCounterIndex = 0
+        deniedCounterIndex = 0
+        deniedFoodCounter.text = String(("Rejected: \(approvedCounterIndex)/16"))
+        foodCounter.text = String(("Maybe: \(deniedCounterIndex)/16"))
+        print (foodChoices)
+        print(foodChoices.count)
+    }
+    
     @IBOutlet weak var restartWholeApp: UIBarButtonItem!
     @IBOutlet weak var upButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
@@ -201,6 +287,7 @@ class ViewController: UIViewController {
         }
         
         generateAnswer()
+        //print (index)
         // Increment the index
         lastChoice = Choice(operation:.right, value: nil)
         //currIndex += 1
@@ -233,7 +320,7 @@ class ViewController: UIViewController {
             print("DELETE ITEM: \(deleteItem)")
             self.deletedItem = deleteItem
             resetChoices.isEnabled = true
-
+        
         //FIXME: fix this
         if deniedCounterIndex + approvedCounterIndex < 16 {
             deniedCounterIndex += 1
@@ -320,7 +407,11 @@ class ViewController: UIViewController {
         foodCategory.text = foodChoices[index]
 
     }
-    
+    func fullFoodChoices() {
+        
+        let foodChoices = ["Chinese", "Mexican", "Italian", "Japanese", "Mediterranean", "French", "Thai", "Spanish", "Indian", "Greek", "BBQ", "Burgers", "Sandwich", "Seafood", "Pizza", "Steak"]
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let identifier = segue.identifier else { return }
         switch identifier {
